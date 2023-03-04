7c72552dd3286db2cbb80dc7514b7be1ff05853f
Gravewalker666 <heshanandrews99@gmail.com>
2023-03-04 12:29:54 +0530

    Write predict endpoint with basic funcitonalities

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from joblib import load
+
+model = load('model/model.joblib')
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/predict/<float:sepal_length>/<float:sepal_width>/<float:petal_length>/<float:petal_width>')
+def predict(sepal_length, sepal_width, petal_length, petal_width):
+    return str(model.predict([[sepal_length, sepal_width, petal_length, petal_width]])[0])
+
+
+if __name__ == '__main__':
+    app.run()
