1e03928e3ef4da2bad3db92c36ae59e214a8dcf7
Gravewalker666 <heshanandrews99@gmail.com>
2023-03-05 12:45:33 +0530

    Add css styles to the UI

---
+++ client/index.js
@@ -0,0 +1,17 @@
+const form = document.getElementById('flower-form');
+const flowerContainer = document.getElementById('flower-type-prediction');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const response = await fetch('http://127.0.0.1:5000/predict', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(Object.fromEntries(formData))
+  });
+  const { flower_type } = await response.json();
+  flowerContainer.innerText = flower_type;
+});
